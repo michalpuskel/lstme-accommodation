eb1beede75f75f6b00ddf1c1e1550a72a24c9690
Michal Puškel <michal.puskel@gmail.com>
2019-04-29 12:15:14 +0200

    login form

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const LoginScreen = () => {
-  return <div>LoginScreen.</div>;
-};
+class LoginScreen extends Component {
+  state = {
+    emailInput: "",
+    passwordInput: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          email:
+          <input
+            type="email"
+            value={this.state.emailInput}
+            onChange={this.handleInputChange}
+            name="emailInput"
+            placeholder="meno@email.sk"
+            required
+          />
+        </label>
+        <label>
+          heslo:
+          <input
+            type="password"
+            value={this.state.passwordInput}
+            onChange={this.handleInputChange}
+            name="passwordInput"
+            placeholder="*****"
+            required
+          />
+        </label>
+        <input type="submit" value="Prihlásiť" />
+        <Link to="/registration">Registrácia</Link>
+      </form>
+    );
+  }
+}
 
 export default LoginScreen;
