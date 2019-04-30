362e3b14b66ef434e66c8d5408003ed52e7edd2a
Michal Puškel <michal.puskel@gmail.com>
2019-04-30 17:20:53 +0200

    dotenv firebase keys

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./components/App";
+require("dotenv").config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
