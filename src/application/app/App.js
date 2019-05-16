2b072d965f97a09609ea142def634188cabf24f3
Michal Puškel <michal.puskel@gmail.com>
2019-05-16 18:03:23 +0200

    refactored router

--- src/application/app/App.js
+++ src/application/app/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
 
-const App = () => {
-  return <div> app </div>;
+import "./App.css";
+import UserContext from "../../config/UserContext";
+import Router from "../router/Router";
+
+const App = props => {
+  return (
+    <UserContext.Provider value={props.user}>
+      <Router />
+    </UserContext.Provider>
+  );
 };
 
 export default App;
---
+++ src/application/router/Router.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import ProtectedRoute from "./ProtectedRoute";
+import PrivateRoute from "./PrivateRoute";
+import UserContext from "../../config/UserContext";
+
+import Auth from "../../pages/auth/Auth";
+import Rooms from "../../pages/rooms/Rooms";
+import RoomDetail from "../../pages/roomDetail/RoomDetail";
+import Users from "../../pages/users/Users";
+import UserDetail from "../../pages/userDetail/UserDetail";
+import NotFound from "../../pages/notFound/NotFound";
+
+const Router = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <Switch>
+      <ProtectedRoute
+        exact
+        path="/"
+        component={Rooms}
+        condition={user}
+        redirect="/auth"
+      />
+      <ProtectedRoute
+        path="/auth"
+        component={Auth}
+        condition={!user}
+        redirect="/"
+      />
+      <ProtectedRoute
+        path="/room/:roomId"
+        component={RoomDetail}
+        condition={user}
+        redirect="/auth"
+      />
+      <ProtectedRoute
+        path="/users"
+        component={Users}
+        condition={user && user.is_super_admin}
+        redirect="/auth"
+      />
+      <PrivateRoute
+        path="/user/:userId"
+        component={UserDetail}
+        condition={user}
+        redirect="/auth"
+        privateId={user.uid}
+      />
+      <Route component={NotFound} />
+    </Switch>
+  );
+};
+
+export default Router;
