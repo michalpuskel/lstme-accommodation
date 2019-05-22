446efe5afe67a0571c503dd431a0701973dfef35
Michal Puškel <michal.puskel@gmail.com>
2019-05-22 22:42:52 +0200

    delete authed user

---
+++ src/hooks/user/useUserDelete.js
@@ -0,0 +1,35 @@
+import { useCallback } from "react";
+
+import { auth, database } from "../../config/firebase";
+
+const useUserDelete = userId => {
+  const userDelete = useCallback(async () => {
+    const userRef = database.collection("users").doc(userId);
+
+    try {
+      //TODO transaction begin
+      await database.runTransaction(async transaction => {
+        const userDoc = await transaction.get(userRef);
+        const { room_id } = userDoc.data();
+        if (room_id) {
+          const bedRef = database
+            .collection("rooms")
+            .doc(room_id)
+            .collection("beds")
+            .doc(userId);
+          transaction.delete(bedRef);
+        }
+        transaction.delete(userRef);
+      });
+
+      await auth.currentUser.delete(); // TODO delete any user
+      //TODO transaction end
+    } catch (error) {
+      console.error(error);
+    }
+  }, [userId]);
+
+  return userDelete;
+};
+
+export default useUserDelete;
--- src/pages/userDetail/UserDetail.js
+++ src/pages/userDetail/UserDetail.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import useUserDelete from "../../hooks/user/useUserDelete";
 import Layout from "../../application/layout/layout/Layout";
 
-const UserDetail = () => {
+const UserDetail = props => {
+  const userDelete = useUserDelete(props.match.params.userId);
+
   return (
     <Layout title="Nastavenia používateľa">
-      <div>UserDetail</div>
+      <button onClick={userDelete}>Vymazať konto</button>
     </Layout>
   );
 };
