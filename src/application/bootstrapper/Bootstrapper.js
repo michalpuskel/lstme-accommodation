7d621b8c76718a077f479b4234b03213c49e56df
Michal Puškel <michal.puskel@gmail.com>
2019-05-25 01:08:36 +0200

    loading

--- src/application/bootstrapper/Bootstrapper.js
+++ src/application/bootstrapper/Bootstrapper.js
@@ -3,17 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import useAuthedUser from "../../hooks/user/useAuthedUser";
 import App from "../app/App";
+import Loading from "../../lib/loading/Loading";
 
 const Bootstrapper = () => {
   const authedUser = useAuthedUser();
 
   return (
     <BrowserRouter>
-      {authedUser === undefined ? (
-        <div>Loading...</div>
-      ) : (
-        <App user={authedUser} />
-      )}
+      {authedUser === undefined ? <Loading /> : <App user={authedUser} />}
     </BrowserRouter>
   );
 };
---
+++ src/lib/loading/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="hero-body">
+      <div className="container">
+        <div className="level">
+          <div className="level-item">
+            <span className="button is-primary is-loading">Loading</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
