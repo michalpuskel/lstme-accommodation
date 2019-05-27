a39d3a403ee76a6b85c0f082ed9d6bcfdb3e8168
Michal Puškel <michal.puskel@gmail.com>
2019-05-27 17:42:06 +0200

    swap sent styled

--- src/lib/notification/swapSent/SwapSent.js
+++ src/lib/notification/swapSent/SwapSent.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,20 +8,30 @@ const SwapSent = props => {
   const userName = useUserName();
 
   return (
-    <div>
-      <h1>Výmena postele</h1>
-      <p>
-        Žiadaš účastníka <strong>{userName(props)}</strong> z izby{" "}
-        <Link to={`/room/${props.roomSwapSentTo.uid()}`}>
-          <em>{props.roomSwapSentTo.name()}</em>
-        </Link>{" "}
-        o výmenu nocľahu. Ty bývaš na izbe{" "}
-        <Link to={`/room/${props.roomMy.uid()}`}>
-          <em>{props.roomMy.name()}</em>
-        </Link>
-        .
-      </p>
-      <button onClick={props.onSwapCancel}>Zrušiť žiadosť</button>
+    <div className="card notification--ring notifications__message">
+      <header className="card-header">
+        <p className="card-header-title">Výmena postele</p>
+      </header>
+
+      <div className="card-content">
+        <div className="content">
+          Žiadaš účastníka <strong>{userName(props)}</strong> z izby{" "}
+          <Link to={`/room/${props.roomSwapSentTo.uid()}`}>
+            <em>{props.roomSwapSentTo.name()}</em>
+          </Link>{" "}
+          o výmenu nocľahu. Ty bývaš na izbe{" "}
+          <Link to={`/room/${props.roomMy.uid()}`}>
+            <em>{props.roomMy.name()}</em>
+          </Link>
+          .
+        </div>
+      </div>
+
+      <footer className="card-footer">
+        <a href="#" className="card-footer-item" onClick={props.onSwapCancel}>
+          Zrušiť žiadosť
+        </a>
+      </footer>
     </div>
   );
 };
