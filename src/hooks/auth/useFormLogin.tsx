eb35d4d9319d1be41eed7bc225ee1bfe093193c2
Michal Puškel <puskel@freevision.sk>
2019-06-21 17:47:57 +0200

    Feedback useFormLogin.

--- src/hooks/auth/useFormLogin.tsx
+++ src/hooks/auth/useFormLogin.tsx
@@ -23,16 +23,36 @@ const useFormLogin = (): IFormLogin => {
   // const hash = {};
   // const formLogin = Object.keys(fields).forEach(key => {});
 
-  const formLogin = Object.keys(fields).map(
-    (field: string): IFormField => ({
-      name: field,
-      value: fields[field as keyof IFormLoginState],
-      onChange: changeHandler,
-      id: `${field}Input`
-    })
+  // const toHashMap = (form: IFormField[]): IForm =>
+  //   form.reduce((acc: IForm, v: IFormField): IForm => {
+  //     acc[v.name] = v;
+  //     return acc;
+  //   }, {});
+
+  const formLogin = Object.keys(fields).reduce(
+    (form: IFormLogin, field: keyof IFormLogin): IFormLogin => {
+      // (form: IFormLogin, field: string): IFormLogin => {
+
+      form["lb"] = {
+        name: "field",
+        value: "val",
+        onChange: changeHandler,
+        id: `${field}Input`
+      };
+
+      form[field as string] = {
+        name: field,
+        value: fields[field as keyof IFormLoginState],
+        onChange: changeHandler,
+        id: `${field}Input`
+      };
+      return form;
+    },
+    {}
   );
 
-  return toHashMap(formLogin) as IFormLogin;
+  // return formLogin;
+  return formLogin as IFormLogin;
 };
 
 export default useFormLogin;
