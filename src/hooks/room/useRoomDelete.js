a6533df89fdfaeb0cdd49b0ff2352c09dee78e5b
Michal Puškel <puskel@freevision.sk>
2019-08-05 15:07:13 +0200

    Replace firebase transaction to sequential commands to work around sudden misbehaviour of firebase in room delete.

--- src/hooks/room/useRoomDelete.js
+++ src/hooks/room/useRoomDelete.js
@@ -12,54 +12,52 @@ const useRoomDelete = (roomId, accommodatedUsers) => {
       .collection("beds");
 
     try {
-      await database.runTransaction(async transaction => {
-        const swappingUsers = [];
+      const swappingUsers = [];
 
-        Object.keys(accommodatedUsers).map(async userId => {
-          const userRef = database.collection("users").doc(userId);
-          const userDoc = await transaction.get(userRef);
-          const { swap_sent_to_id, swap_received_from_id } = userDoc.data();
+      Object.keys(accommodatedUsers).map(async userId => {
+        const userRef = database.collection("users").doc(userId);
+        const userDoc = await userRef.get();
+        const { swap_sent_to_id, swap_received_from_id } = userDoc.data();
 
-          const swapPartnerId = swap_sent_to_id || swap_received_from_id;
-          if (swapPartnerId) {
-            swappingUsers.push(userId);
-            swappingUsers.push(swapPartnerId);
-          }
-        });
-
-        const roomDoc = await transaction.get(roomRef);
-        const room = roomDoc.data();
-
-        swappingUsers.map(userId => {
-          const userRef = database.collection("users").doc(userId);
-          transaction.update(userRef, {
-            swap_sent_to_id: null,
-            swap_received_from_id: null
-          });
-
-          const notificationRef = database
-            .collection("notifications")
-            .doc(userId)
-            .collection("denials")
-            .doc();
-          transaction.set(notificationRef, {
-            uid: notificationRef.id,
-            room: room.name,
-            type: "room-delete",
-            timestamp: dbTimestamp
-          });
-          return userRef;
-        });
-
-        Object.keys(accommodatedUsers).map(userId => {
-          const userRef = database.collection("users").doc(userId);
-          transaction.update(userRef, { room_id: null });
-          transaction.delete(bedsRef.doc(userId));
-          return userRef;
-        });
-
-        transaction.delete(roomRef);
+        const swapPartnerId = swap_sent_to_id || swap_received_from_id;
+        if (swapPartnerId) {
+          swappingUsers.push(userId);
+          swappingUsers.push(swapPartnerId);
+        }
       });
+
+      const roomDoc = await roomRef.get();
+      const room = roomDoc.data();
+
+      swappingUsers.map(async userId => {
+        const userRef = database.collection("users").doc(userId);
+        await userRef.update({
+          swap_sent_to_id: null,
+          swap_received_from_id: null
+        });
+
+        const notificationRef = database
+          .collection("notifications")
+          .doc(userId)
+          .collection("denials")
+          .doc();
+        await notificationRef.set({
+          uid: notificationRef.id,
+          room: room.name,
+          type: "room-delete",
+          timestamp: dbTimestamp
+        });
+        return userRef;
+      });
+
+      Object.keys(accommodatedUsers).map(async userId => {
+        const userRef = database.collection("users").doc(userId);
+        await userRef.update({ room_id: null });
+        bedsRef.doc(userId).delete();
+        return userRef;
+      });
+
+      await roomRef.delete();
     } catch (error) {
       console.error(error);
     }
