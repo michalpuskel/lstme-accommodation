80028fa6ca44be628bf676644681241cabe291c1
Michal Puškel <puskel@freevision.sk>
2020-05-10 16:36:18 +0200

    Change head event title

--- src/lib/nav/navBrand/NavBrand.js
+++ src/lib/nav/navBrand/NavBrand.js
@@ -3,15 +3,20 @@ import { Link } from "react-router-dom";
 
 import NavBurger from "../navBurger/NavBurger";
 import UserContext from "../../../config/UserContext";
+import useEvent from "../../../pages/EventList/useEvent";
 
 const NavBrand = (props) => {
   const user = useContext(UserContext);
   const { event_id } = user || {};
 
+  const event = useEvent(event_id);
+
   return (
     <>
       <Link className="navbar-item" to="/">
-        <h1 className="title">{event_id ? "LSTME" : "Výber organizácie"}</h1>
+        <h1 className="title">
+          {event_id && event ? event.title : "Výber organizácie"}
+        </h1>
       </Link>
       <NavBurger showMenu={props.showMenu} toggleMenu={props.toggleMenu} />
     </>
