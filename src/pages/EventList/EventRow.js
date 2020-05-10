a2f2efdf08a1f5e3f9e3fe04519f701ef73de556
Michal Puškel <puskel@freevision.sk>
2020-05-10 16:07:29 +0200

    Handle event select

--- src/pages/EventList/EventRow.js
+++ src/pages/EventList/EventRow.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { database } from "../../config/firebase";
+import UserContext from "../../config/UserContext";
 import "./EventList.scss";
 
-const EventRow = ({ title, description, image, imageFile }) => {
+const EventRow = ({ title, description, image, imageFile, uid }) => {
+  const user = useContext(UserContext);
+
+  const handleSelect = async (event) => {
+    event.preventDefault();
+
+    const ref = database.collection("users").doc(user.uid);
+    try {
+      await ref.update({
+        event_id: uid,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="box">
+    <div
+      className={`box event-list__row ${
+        user.event_id === uid ? "event-list__row--selected" : ""
+      }`}
+      onClick={handleSelect}
+    >
       <div className="flex justify-between">
         <div className="pr-8">
           <h3 className="title">{title}</h3>
