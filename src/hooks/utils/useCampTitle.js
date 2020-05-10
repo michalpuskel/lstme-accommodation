fbe84d8f1105c26cbecd2fcf924662269eac45ab
Michal Puškel <puskel@freevision.sk>
2020-05-10 16:31:54 +0200

    Change footer event title

--- src/hooks/utils/useCampTitle.js
+++ src/hooks/utils/useCampTitle.js
@@ -1,14 +1,17 @@
 import { useCallback, useContext } from "react";
 import moment from "moment";
 import UserContext from "../../config/UserContext";
+import useEvent from "../../pages/EventList/useEvent";
 
 const useCampTitle = () => {
   const user = useContext(UserContext);
   const { event_id } = user || {};
 
+  const event = useEvent(event_id);
+
   const getCampTitle = useCallback(
-    () => `${moment().format("YYYY")} ${event_id ? "LSTME" : ""}`,
-    [event_id]
+    () => `${moment().format("YYYY")} ${event_id && event ? event.title : ""}`,
+    [event_id, event]
   );
 
   return getCampTitle;
